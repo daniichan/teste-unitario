4c216549350d87f27ba6f06c621007af8b265a43
Daniele Leite <daniele.santos@vixsystem.com.br>
2020-04-30 17:01:48 -0300

    06. Testando um caso de erro

--- alurapic/src/app/shared/validators/lower-case.validator.spec.ts
+++ alurapic/src/app/shared/validators/lower-case.validator.spec.ts
@@ -2,10 +2,13 @@ import { isLowerCase } from "./lower-case.validator";
 
 // Parametros do describe: descrição do teste, e os testes em si
 describe('A função isLowerCase', () => {
-    it('deve confirmar quanto recebe um texto em caixa baixa', () => {
+    it('deve confirmar quando recebe um texto em caixa baixa', () => {
         // Preparação, execução e avaliação são as etapas de um teste
         const valor = 'daniele'; // Preparação
         const resultado = isLowerCase(valor); // Execução
         expect(resultado).toBeTruthy; // Avaliação (toBeTruthy => que seja verdadeiro)
-    })
+    });
+    it('deve validar quando o valor enviado não for caixa baixa', () => {
+        expect(isLowerCase('Daniele')).toBeFalsy(); // tem q ser falso
+    });
 }) 

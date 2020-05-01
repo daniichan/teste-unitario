7cb9cf7ae39fe7c5d3720264f293e41f7d71b3fc
Daniele Leite <daniele.santos@vixsystem.com.br>
2020-05-01 16:54:45 -0300

    5.4. Simulando uma situação de Erro

--- alurapic/src/app/home/sign-up/signup.component.spec.ts
+++ alurapic/src/app/home/sign-up/signup.component.spec.ts
@@ -6,7 +6,7 @@ import { RouterTestingModule } from "@angular/router/testing";
 import { ReactiveFormsModule } from "@angular/forms";
 import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { Router } from "@angular/router";
-import { of } from "rxjs";
+import { of, throwError } from "rxjs";
 import { VMessageModule } from "src/app/shared/componets/vmessage/vmessage.module";
 
 describe('O formulário SignUp', () => {
@@ -52,10 +52,24 @@ describe('O formulário SignUp', () => {
         component.signupForm.get('fullName').setValue('Daniele Leite');
         component.signupForm.get('userName').setValue('daniele');
         component.signupForm.get('password').setValue('123');
-
         component.signUp();
         
         expect(navigateSpy).toHaveBeenCalledWith(['']);
+    });
+
+    it('deve realizar o log caso ocorra algum erro', () => {
+        spyOn(signupService, 'signup').and.returnValue(throwError('Erro de Servidor')); // to retornando um observable de erro
+        
+        component.signupForm.get('email').setValue('daniele@daniele.com');
+        component.signupForm.get('fullName').setValue('Daniele Leite');
+        component.signupForm.get('userName').setValue('daniele');
+        component.signupForm.get('password').setValue('123');
+
+        const spyLog = spyOn(console, 'log');
+        
+        component.signUp();
+
+        expect(spyLog).toHaveBeenCalledWith('Erro de Servidor');
     });
 
 });

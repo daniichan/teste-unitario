78f490aa759d01103d3cb50f533eaa8e1a63810a
Daniele Leite <daniele.santos@vixsystem.com.br>
2020-05-01 15:40:50 -0300

    4.3. TestBed assíncrono

--- alurapic/src/app/core/footer/footer.component.spec.ts
+++ alurapic/src/app/core/footer/footer.component.spec.ts
@@ -1,16 +1,30 @@
 import { TestBed } from "@angular/core/testing";
+import { RouterTestingModule } from '@angular/router/testing';
 import { FooterComponent } from "./footer.component";
+import { UserService } from "../user/user.service";
+import { of } from "rxjs";
 
 describe('O componente Footer', () => {
 
     let component: FooterComponent;
 
-    beforeEach(() => {
+    // boas praticas do angular, pede para criar dois beforeeach 1 async e outro nao
+    beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [ FooterComponent ], // COMPONENTS
-            // providers: [] SERVICES,
-            // imports: [] MODULES
-        });
+            // routertestingmodule testa rotas
+            imports: [ RouterTestingModule ], // MODULES
+            providers: [ UserService ] // SERVICES,
+        }).compileComponents(); // boa pratica. ele é assincrono
+    }));
+    
+    beforeEach(() => {
+        const userService = TestBed.get(UserService);
+        spyOn(userService, 'getUser').and.returnValue(of({
+            email: 'danii.exe@gmail.com',
+            name: 'Daniele',
+            id: 1
+        }));
 
         const fixture = TestBed.createComponent(FooterComponent); // Para criar o componente
         component = fixture.componentInstance; // Extrair o componente

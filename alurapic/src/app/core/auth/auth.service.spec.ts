a1c6af9e12c34066f5e2f2701bf691c73318b7c5
Daniele Leite <daniele.santos@vixsystem.com.br>
2020-05-01 15:14:30 -0300

    3.5. Funções assíncronas

--- alurapic/src/app/core/auth/auth.service.spec.ts
+++ alurapic/src/app/core/auth/auth.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from "@angular/core/testing";
+import { TestBed, fakeAsync, tick } from "@angular/core/testing";
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { AuthService } from "./auth.service";
 import { UserService } from "../user/user.service";
@@ -27,7 +27,8 @@ describe('O serviço AuthService', () => {
         expect(service).toBeTruthy();
     });
 
-    it('deve autenticar o usuário', () => {
+     // FAKEASYNC cria uma bolha que permite simular a passagem de tempo de resposta do servidor 
+    it('deve autenticar o usuário', fakeAsync(() => {
         const fakeBody = {
             id: 1,
             nome: 'Daniele',
@@ -37,13 +38,13 @@ describe('O serviço AuthService', () => {
         // spy uma função que cria um duble da minha classe/metodo q nao quero usar, no caso para o setToken do UserService
         // no momento que o flush fazer o retorno da requisição, vai cair no setToken (isso no service), ai ele não chamará o setToken de verdade
         //     chamará o duble
-        spyOn(userService, 'setToken').and.returnValue(null);
+        const spy = spyOn(userService, 'setToken').and.returnValue(null);
 
         service
             .authenticate('daniele', '1234')
             .subscribe(response => {
                 expect(response.body).toEqual(fakeBody);
-                expect(response.headers.get('x-access-token')).toBe('tokenTest');
+                expect(spy).toHaveBeenCalledWith('tokenTest'); // verifica se as funções foram chamadas.
             });
         
         //  FAZ A REQUISICAO avalia se algum metodo do teste executou algum requisicao HTTP (rest)
@@ -55,5 +56,7 @@ describe('O serviço AuthService', () => {
         request.flush(fakeBody, { 
             headers: { 'x-access-token': 'tokenTest' }
         })
-    });
+
+        tick(); // simula a passagem de tempo de requisição ao server
+    }));
 });
